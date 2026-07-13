ffeebb8bbc53b35488df677692a83d757675a3fc
Aman-Thewhiz <amanranju23@gmail.com>
2026-07-13 21:20:42 +0530

    Seed production database

--- server/server.js
+++ server/server.js
@@ -3,9 +3,20 @@ import { connectDatabase } from './config/db.js';
 import { connectRedis } from './config/redis.js';
 import { env } from './config/env.js';
 
+import { seedUsers } from './utils/seedUsers.js';
+import { seedCourseCategories } from './utils/seedCourseCategories.js';
+import { seedLeavePolicies } from './utils/seedLeavePolicies.js';
+
 async function bootstrap() {
   await connectDatabase();
   await connectRedis();
+
+ 
+  await seedUsers();
+  await seedCourseCategories();
+  await seedLeavePolicies();
+  console.log("Database seeded successfully.");
+
   app.listen(env.PORT, () => {
     console.log(`Server running on port ${env.PORT}`);
   });
